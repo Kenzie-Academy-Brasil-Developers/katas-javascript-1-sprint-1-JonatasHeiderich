4cd3ddc1f0313513b083999db87218381d91d25d
Jonatas Heiderich <jonatas.heiderich@yahoo.com>
2021-08-10 15:33:01 -0300

    Exercicio terminado e formatado

--- katas1.js
+++ katas1.js
@@ -1,103 +1,107 @@
 function oneThroughTwenty() {
   let meuRetorno = [];
- for (let counter = 1; counter <= 20; counter++) {
-   meuRetorno.push(counter)
- }
- return meuRetorno
+   for (let counter = 1; counter <= 20; counter++){
+        meuRetorno.push(counter)
+   }
+  return meuRetorno
 }
-console.log(oneThroughTwenty());
+console.log(oneThroughTwenty())
 
 
 function evensToTwenty() {
   let meuRetorno1 = [];
- for (let counter = 1; counter <= 20; counter++) {
-  if (counter % 2 ===0);
-    meuRetorno1.push(counter)
-  } 
+  for (let counter = 1; counter <= 20; counter++) {
+        if (counter % 2 ===0);
+            meuRetorno1.push(counter)
+    } 
     return meuRetorno1
 }
   console.log(evensToTwenty());
 
 
-function oddsToTwenty() {
-  let meuRetorno2 = [];
- for (let counter = 1; counter <= 20; counter++) {
- if (counter % 2 !==0)
-  meuRetorno2.push(counter)
+  function oddsToTwenty() {
+    let meuRetorno2 = [];
+    for (let counter = 1; counter <=20; counter++){
+        if (counter % 2 !== 0)
+            meuRetorno2.push(counter)
+  }
+  return meuRetorno2
 }
-return meuRetorno2
-}
-console.log(oddsToTwenty());
+console.log(oddsToTwenty())
+
 
 
 function multiplesOfFive() {
   let meuRetorno3 = [];
-  for (let counter = 1; counter <= 100; counter++) {
-  if (counter % 5 ===0 || counter % 5 === 5)
-  meuRetorno3.push(counter)
- }
- return meuRetorno3
+  for (let counter = 1; counter <= 100; counter ++){
+      if (counter % 5 === 0 || counter % 5 === 5)
+          meuRetorno3.push(counter)
+  }
+  return meuRetorno3
 }
 console.log(multiplesOfFive());
 
 
+
 function squareNumbers() {
   let meuRetorno4 = [];
- for (let counter = 1; counter <= 100; counter++) {
- let qP = counter * counter
- if (qP <=100){
-  meuRetorno4.push(qP)
- }
- }
- return meuRetorno4
+  for (let counter = 1; counter <= 100; counter++){
+      let qP = counter * counter 
+      if (qP <= 100){
+          meuRetorno4.push(qP)
+      }
+  }
+  return meuRetorno4
 }
 console.log(squareNumbers())
 
 
 
 function countingBackwards() {
-  
-    let meuRetorno5 = [];
-   for (let counter = 20; counter > 0; counter--){
-   meuRetorno5.push(counter)
-   }
-   return meuRetorno5;
-   }
-   console.log(countingBackwards());
+  let meuRetorno5 = [];
+  for (let counter = 20; counter > 0; counter --){
+      meuRetorno5.push(counter)
+  }
+  return meuRetorno5;
+}
+console.log(countingBackwards())
+
   
 
 function evenNumbersBackwards() {
-  let meuRetorno6 = [];
- for (let counter = 20; counter > 0; counter--) {
- if (counter % 2 ===0)
-  meuRetorno6.push(counter)
- }
-  return meuRetorno6
-}
-console.log(evenNumbersBackwards());
-
-
-
-  function oddNumberBackwards(){
-    let meuRetorno7 = []
+    let meuRetorno6 = []
     for (let counter = 20; counter > 0; counter--){
-    if (counter % 2 !== 0)
-    meuRetorno7.push(counter)
+        if(counter % 2 === 0)
+            meuRetorno6.push(counter)
     }
-    return meuRetorno7
-    }
-    console.log(oddNumberBackwards())
+    return meuRetorno6
+}
+console.log(evenNumbersBackwards())
+
+
+
+function oddNumberBackwards(){
+  let meuRetorno7 = [];
+  for (let counter = 20; counter > 0; counter--){
+      if (counter % 2 !== 0)
+          meuRetorno7.push(counter)
+  }
+  return meuRetorno7
+}
+console.log (oddNumberBackwards())
    
 
-    function multiplesOfFiveBackwards() {
-      let meuRetorno8 = [];
-      for (let counter = 100; counter > 0; counter--){
-          if(counter % 5 === 0 || counter % 5 === 5)
-              meuRetorno8.push(counter)
-      }
-      return meuRetorno8
+
+function multiplesOfFiveBackwards() {
+  let meuRetorno8 = [];
+   for (let counter = 100; counter > 0; counter--){
+      if(counter % 5 === 0 || counter % 5 === 5)
+          meuRetorno8.push(counter)
   }
-  console.log(multiplesOfFiveBackwards());
+  return meuRetorno8
+}
+console.log(multiplesOfFiveBackwards());
+
 
 
 function squareNumbersBackwards(){
